7d0d52f29516129180270061026e5914617680e3
Peter Segal <psegal@zoomcare.com>
2018-11-22 12:03:17 -0800

    refactor index for genre

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const Joi = require('joi');
 const genres = require('./routes/genre');
 
 const app = express();
 app.use(express.json());
 app.use('/api/genres', genres);
 
-function validateGenre(g) {
-    const schema = {
-        id: Joi.number(), // Added this so that we can do put without uri params
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(g, schema);
-}
 
 // This is how you set a port from an environment variable
 const port = process.env.PORT || 3000;
