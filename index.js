2ed80dfa1d88e98dd733f55c9ddf9f6c9f2c9ebf
Peter <pesegal@gmail.com>
2018-11-24 17:37:12 -0800

    added my attempt at async db

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const genres = require('./routes/genre');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
 app.use('/api/genres', genres);
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error("Could not connect to mongoDB."));
 
 
 // This is how you set a port from an environment variable
--- routes/genre.js
+++ routes/genre.js
@@ -2,47 +2,73 @@ const express = require('express');
 const routes = express.Router();
 const Joi = require('joi');
 
-// Example in memory data structure for simplicity.
-let genres = [
-    { id: 1, name: 'Classical' },
-    { id: 2, name: 'Jazz' },
-    { id: 3, name: 'Rock' }
-]
+const mongoose = require('mongoose');
+
+
+const genreSchema = new mongoose.Schema({
+    name: { type:String, required: true, minlength: 3 }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 routes.get('/', (req, res) => {
-    res.send(genres);
+    async function getGenres() {
+        const genres = await Genre.find().select({ _id:1, name:1 });
+        return res.send(genres);
+    }
+    getGenres();
 });
 
 routes.post('/', (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    };
-    genres.push(genre);
-    res.send(genre);
+    // Create in database
+    async function createGenre(body) {
+        const genre = new Genre({
+            name: body.name
+        });
+        try {
+            const result = await genre.save();
+            res.send(result);
+        }
+        catch (err) {
+            res.send(err);
+        }
+    }
+    createGenre(req.body);
 });
 
-routes.put('/', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.body.id));
-    if (!genre) return res.status(404).send(`No genre with id: ${req.body.id} found.`);
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error);
-    genre.name = req.body.name;
-    res.send(genre);    
+routes.put('/:id', (req, res) => {
+    async function updateGenre(id, body) {
+        // find object
+        const genre = await Genre.findById(id);
+        if (!genre) return new Error(`No genre with id: ${id} found.`);
+        // validate body
+        const { error } = validateGenre(body);
+        if (error) return res.status(400).send(error);
+        // Update item
+        genre.set(body);
+        try {
+            res.send(await genre.save());
+        }
+        catch(err) {
+            res.send(err);
+        }        
+    }
+    updateGenre(req.params.id, req.body);    
 });
 
 routes.delete('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send(`No genre with id: ${req.params.id} found.`);
-    genres.splice(genres.indexOf(genre), 1); // note: splice, not slice
-    res.send(genre);
+    async function removeGenre(id) {           
+        const result = await Genre.deleteOne({ _id: id });
+        if (!result) return res.status(404).send(`No genre with id: ${id} found.`);
+        res.send(result);
+    }
+    removeGenre(req.params.id);
 });
 
 function validateGenre(g) {
     const schema = {
-        id: Joi.number(), // Added this so that we can do put without uri params
         name: Joi.string().min(3).required()
     };
     return Joi.validate(g, schema);
