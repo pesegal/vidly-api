534029a2a1a88e0c8cbdd3ac73c98013bb08f71d
Peter <pesegal@gmail.com>
2018-12-04 16:50:04 -0800

    add endpoint middleware and fixed model ref

--- routes/genre.js
+++ routes/genre.js
@@ -1,18 +1,19 @@
+const auth = require('../middleware/auth');
 const express = require('express');
 const routes = express.Router();
 const mongoose = require('mongoose');
-const { Movie, validate } = require('../models/genre');
+const { Genre, validate } = require('../models/genre');
 
 routes.get('/', async (req, res) => {
-    const genres = await Movie.find().select({ _id:1, name:1 });
+    const genres = await Genre.find().select({ _id:1, name:1 });
     return res.send(genres);
 });
 
-routes.post('/', async (req, res) => {
+routes.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     // Create in database
-    const genre = new Movie({
+    const genre = new Genre({
         name: req.body.name
     });
     try {
@@ -28,19 +29,19 @@ routes.put('/:id', async (req, res) => {
     // validate body
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error);
-    const genre = await Movie.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
     if (!genre) return res.status(404).send(Error(`No genre with id: ${req.params.id} found.`));
     res.send(genre);
 });
 
 routes.delete('/:id', async (req, res) => {         
-    const genre = await Movie.findByIdAndDelete(req.params.id);
+    const genre = await Genre.findByIdAndDelete(req.params.id);
     if (!genre) return res.status(404).send(`No genre with id: ${req.params.id} found.`);
     res.send(genre);
 });
 
 routes.get('/:id', async (req, res) => {
-    const genre = await Movie.findById(req.params.id);
+    const genre = await Genre.findById(req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID not found');
     res.send(genre);
 })
