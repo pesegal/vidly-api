7632a4b90be5f183bc9a9b5335503a88339c57ee
Peter <pesegal@gmail.com>
2018-12-06 17:50:23 -0800

    add MongoDB level logging info

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const config = require('config');
 const error = require('./middleware/error');
 
 winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
+winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly', level: 'error' });
 
 const app = express();
 app.use(express.json());
